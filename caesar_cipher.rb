62affcd091bcf9f166e9e7f445c28fa0d40d6ab1
j-unisa <104635026+j-unisa@users.noreply.github.com>
2026-01-20 10:48:26 -0800

    refactor: reorder methods and clean up comments
    - Move main caesar_cipher method to top of file
    - Simplify inline comments for better readability
    - Remove verbose pseudo-code descriptions

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,23 @@
+def caesar_cipher(msg, num)
+
+  # Split message into an array
+  characters = msg.split("")
+
+  ascii_converted_characters = convert_to_ascii(characters)
+
+  encrypted_characters = encrypt_characters(ascii_converted_characters, num)
+  
+  encrypted_characters.join
+end
+
 def convert_to_ascii(arr)
   alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
   arr.map do |char|
+    # For letters
     if alpha.include?(char)
       char.ord
+    # For non-alpha characters
     else
       char
     end
@@ -12,40 +26,21 @@ end
 
 def encrypt_characters(arr, num)
   arr.map do |char|
+    # For characters that need to be encrypted
     if char.is_a?(Integer)
-      # Loop from z to a
+      # Wrap from Z/z to A/a
       if ((char + num) > 90) && (char.chr == char.chr.upcase) ||
         ((char + num > 122)) && (char.chr == char.chr.downcase)
         char -= 26 - num
         char.chr
+      # No wrapping needed
       else
         char += num
         char.chr
       end
+    # For characters that don't need to be encrypted
     else
       char
     end
   end
 end
-
-def caesar_cipher(msg, num)
-
-  # Split msg into an array
-  characters = msg.split("")
-
-  # If character is a letter
-  # Then convert character to ASCII using .ord
-  # Else return character
-  ascii_converted_characters = convert_to_ascii(characters)
-
-  # If element is a number
-  # Then add num to converted character
-  # And convert back to character using .chr
-  # Else return character
-  encrypted_characters = encrypt_characters(ascii_converted_characters, num)
-  
-  # Join characters into a string
-  encrypted_characters.join
-end
-
-p caesar_cipher("Hello world Zz!", 1)
