fe3f7f1005834c2bb47819a720bd8096717b9659
j-unisa <104635026+j-unisa@users.noreply.github.com>
2026-01-20 10:28:29 -0800

    refactor: extract ascii conversion to helper
    - Create convert_to_ascii method to handle initial character mapping
    - Simplify caesar_cipher by calling new helper method
    - Maintain existing wrapping and shift logic in main function

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,16 @@
-def caesar_cipher(msg, num)
+def convert_to_ascii(arr)
   alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  arr.map do |char|
+    if alpha.include?(char)
+      char.ord
+    else
+      char
+    end
+  end
+end
+
+def caesar_cipher(msg, num)
 
   # Split msg into an array
   characters = msg.split("")
@@ -7,19 +18,12 @@ def caesar_cipher(msg, num)
   # If character is a letter
   # Then convert character to ASCII using .ord
   # Else return character
-  ascii__converted_characters = characters.map do |char|
-    if alpha.include?(char)
-      char.ord
-    else
-      char
-    end
-  end
 
   # If element is a number
   # Then add num to converted character
   # And convert back to character using .chr
   # Else return character
-  encrypted_characters = ascii__converted_characters.map do |char|
+  encrypted_characters = convert_to_ascii(characters).map do |char|
     if char.is_a?(Integer)
       # Loop from z to a
       if ((char + num) > 90) && (char.chr == char.chr.upcase) ||
@@ -39,4 +43,4 @@ def caesar_cipher(msg, num)
   encrypted_characters.join
 end
 
-caesar_cipher("Hello world Zz!", 1)
+p caesar_cipher("Hello world Zz!", 1)
